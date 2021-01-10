fc3ec8f6f9472c3416a00038efe3705eb4a18fd5
M-Earl <45799848+M-Earl@users.noreply.github.com>
2021-01-09 18:14:39 -0600

    Add files via upload

---
+++ fourierAnalysisStockScripts.js
@@ -0,0 +1,56 @@
+function smallSidebarOpenClose() {
+	if (document.getElementById("smallSidebar").style.maxHeight == "25rem")
+	{
+		document.getElementById("smallSidebar").style.maxHeight = "0rem";
+	}
+	else
+	{
+		document.getElementById("smallSidebar").style.maxHeight = "25rem";
+	}
+}
+
+$(document).ready(function() {
+
+	$('.fadein').each( function(i){
+
+		var bottomObjectAdjusted = $(this).position().top + $(this).outerHeight() - 250;
+		var bottomWindow = $(window).scrollTop() + $(window).height();
+
+		/* If the object is completely visible in the window, fade it it */
+		if( bottomWindow > bottomObjectAdjusted ){
+
+			$(this).animate({'opacity':'1'},500);
+
+		}
+
+	}); 
+	
+	/* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+        /* Check the location of each desired element */
+        $('.fadein').each( function(i){
+
+            var bottomObjectAdjusted = $(this).position().top + $(this).outerHeight() - 200;
+            var bottomWindow = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if( bottomWindow > bottomObjectAdjusted ){
+
+                $(this).animate({'opacity':'1'},500);
+
+            }
+
+        }); 
+
+    });
+	
+	if ($(window).width() < 767) {
+		$('.fadein').each( function(i){
+			$(this).animate({'opacity':'1'},500);
+		});
+	}
+});
+
+$(function(){
+	$("#header").load("header.html");
+});
